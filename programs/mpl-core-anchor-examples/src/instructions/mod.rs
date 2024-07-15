4ee8d5dc8d8a96e207d0511a2a6e078f2a331274
Yannick Tison <ytison@lfdj.com>
2024-07-15 21:00:55 +0000

    remove deposit_jackpot struct & function

--- programs/mpl-core-anchor-examples/src/instructions/mod.rs
+++ programs/mpl-core-anchor-examples/src/instructions/mod.rs
@@ -1,13 +1,11 @@
 pub mod create_collection_v1;
 pub mod create_nft_v1;
-pub mod deposit_jackpot;
 pub mod initialize_jackpot_vault;
 pub mod transfer_v1;
 pub mod withdraw_jackpot;
 
 pub use create_collection_v1::*;
 pub use create_nft_v1::*;
-pub use deposit_jackpot::*;
 pub use initialize_jackpot_vault::*;
 pub use transfer_v1::*;
 pub use withdraw_jackpot::*;
