834d484b2db22c7c1b7c697fda8bf22a41512e31
Yannick Tison <ytison@lfdj.com>
2024-07-16 17:34:33 +0000

    add WINNING_TICKET_ID

--- programs/mpl-core-anchor-examples/src/constants.rs
+++ programs/mpl-core-anchor-examples/src/constants.rs
@@ -3,4 +3,7 @@ use anchor_lang::prelude::*;
 #[constant]
 pub const JACKPOT_SEED: &str = "jackpot_vault";
 pub const JACKPOT_FEES: u64 = 1000000;
-pub const JACKPOT_MAX_TICKETS: u32 = 5;
+pub const JACKPOT_MAX_TICKETS: u32 = 2;
+
+// JACKPOT_WINNING_TICKET_ID must be < JACKPOT_MAX_TICKETS if you want winners :)
+pub const JACKPOT_WINNING_TICKET_ID: u32 = 0;
